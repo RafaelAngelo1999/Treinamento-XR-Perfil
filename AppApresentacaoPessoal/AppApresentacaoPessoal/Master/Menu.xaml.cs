24d7dd6ce589abfd0606541f0ac34f1c2ac410b9
Rafael Angelo <rafaelangelowow@gmail.com>
2020-02-20 14:21:44 -0300

    Treinamento "Semi-Conluido"

---
+++ AppApresentacaoPessoal/AppApresentacaoPessoal/Master/Menu.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppApresentacaoPessoal.Master
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Menu : MasterDetailPage
+	{
+		public Menu ()
+		{
+			InitializeComponent ();
+		}
+         private void GoPageXemarin(object sender, EventArgs e)
+        {
+            Detail = new Pages.Xamarin();
+        }
+        private void GoPagePerfil1(object sender, EventArgs e)
+        {
+            Detail = new Pages.Perfil1();
+        }
+    }
+}
